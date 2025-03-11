22a049f480f660a429d79ad388bfc7129891706e
tom twinkle <tomtinkle29@gmail.com>
2025-03-11 16:18:05 +0900

    Randomize change the time of the test

--- example01/foo_test.go
+++ example01/foo_test.go
@@ -1,6 +1,7 @@
 package example01_test
 
 import (
+	"math/rand/v2"
 	"testing"
 	"testing/synctest"
 	"time"
@@ -8,6 +9,14 @@ import (
 	"github.com/google/uuid"
 	"github.com/tomtwinkle/go-synctest-example/example01"
 )
+
+// Sleep for a random number of minutes and change to the new time.
+// Must used in synctest.Run.
+func changeRandTime(now time.Time) time.Time {
+	d := rand.N(time.Minute)
+	time.Sleep(d)
+	return now.Add(d)
+}
 
 // synctest is an experimental GOEXPERIMENT=synctest must be specified as an environment variable.
 func TestFooService_CreateFoo(t *testing.T) {
@@ -18,6 +27,7 @@ func TestFooService_CreateFoo(t *testing.T) {
 		t.Parallel()
 		synctest.Run(func() {
 			now := time.Now()
+			expectedTime := changeRandTime(now)
 
 			got, err := s.CreateFoo()
 			if err != nil {
@@ -29,10 +39,10 @@ func TestFooService_CreateFoo(t *testing.T) {
 			if got.ID.UUID() == uuid.Nil {
 				t.Errorf("want foo id: not nil, got: %v", got.ID.UUID())
 			}
-			if !got.CreatedAt.Equal(now) {
+			if !got.CreatedAt.Equal(expectedTime) {
 				t.Errorf("want created at: %v, got: %v", now, got.CreatedAt)
 			}
-			if !got.UpdatedAt.Equal(now) {
+			if !got.UpdatedAt.Equal(expectedTime) {
 				t.Errorf("want updated at: %v, got: %v", now, got.UpdatedAt)
 			}
 		})
